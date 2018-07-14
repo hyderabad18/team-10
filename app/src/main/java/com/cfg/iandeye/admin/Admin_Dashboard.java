6e0609b7183b0be29d021d77f85161ab4bf61930
baswarajmamidgi <baswarajmamidgi@gmail.com>
2018-07-14 21:18:23 +0530

    added bottom navigation bars

---
+++ app/src/main/java/com/cfg/iandeye/admin/Admin_Dashboard.java
@@ -0,0 +1,15 @@
+package com.cfg.iandeye.admin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cfg.iandeye.R;
+
+public class Admin_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin__dashboard);
+    }
+}
---
+++ app/src/main/java/com/cfg/iandeye/volunter/Volunteer_Dashboard.java
@@ -0,0 +1,15 @@
+package com.cfg.iandeye.volunter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cfg.iandeye.R;
+
+public class Volunteer_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer__dashboard);
+    }
+}

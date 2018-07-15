8f4a5b09c7c520c613afc3d6682b3e55a8c30592
nanichinnae8 <nanichinnae8@gmail.com>
2018-07-15 06:19:18 +0530

    added statistics in volunteers tab

--- app/src/main/java/com/cfg/iandeye/volunter/Volunteer_Statistics.java
+++ app/src/main/java/com/cfg/iandeye/volunter/Volunteer_Statistics.java
@@ -1,13 +1,27 @@
 package com.cfg.iandeye.volunter;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.cfg.iandeye.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,7 +29,9 @@ import com.cfg.iandeye.R;
  */
 public class Volunteer_Statistics extends Fragment {
     View holder;
-
+    private float[] yData = {45,10,10,35};
+    private String[] xData = {"uproved","accepted","rejected","pending"};
+    PieChart pieChart;
 
     public Volunteer_Statistics() {
         // Required empty public constructor
@@ -27,9 +43,100 @@ public class Volunteer_Statistics extends Fragment {
                              Bundle savedInstanceState) {
         holder=inflater.inflate(R.layout.fragment_volunteer__statistics, container, false);
 
+        pieChart = (PieChart) holder.findViewById(R.id.idPieChart);
 
+        pieChart.setDescription("Sales by employee (In Thousands $) ");
+        pieChart.setRotationEnabled(true);
+        //pieChart.setUsePercentValues(true);
+        //pieChart.setHoleColor(Color.BLUE);
+        //pieChart.setCenterTextColor(Color.BLACK);
+        pieChart.setHoleRadius(25f);
+        pieChart.setTransparentCircleAlpha(0);
+        pieChart.setCenterText("STATUS");
+        pieChart.setCenterTextSize(10);
+        //pieChart.setDrawEntryLabels(true);
+        //pieChart.setEntryLabelTextSize(20);
+        //More options just check out the documentation!
+
+        addDataSet();
+
+        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                Log.d("log", "onValueSelected: Value select from chart.");
+                Log.d("log", "onValueSelected: " + e.toString());
+                Log.d("log", "onValueSelected: " + h.toString());
+
+                int pos1 = e.toString().indexOf("(sum): ");
+                String sales = e.toString().substring(pos1 + 4);
+
+                try {
+                    for (int i = 0; i < yData.length; i++) {
+                        if (yData[i] == Float.parseFloat(sales)) {
+                            pos1 = i;
+                            break;
+                        }
+                    }
+
+                String employee = xData[pos1 + 1];
+                Toast.makeText(getContext(), "Status " + employee + "\n" + "Sales: $" + sales + "K", Toast.LENGTH_LONG).show();
+                }catch (Exception e1)
+                {
+                    e1.getLocalizedMessage();
+                }
+            }
+
+
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
 
         return holder;
+
+    }
+
+    private void addDataSet() {
+        Log.d("log", "addDataSet started");
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+
+        for(int i = 0; i < yData.length; i++){
+            yEntrys.add(new PieEntry(yData[i] , i));
+        }
+
+        for(int i = 1; i < xData.length; i++){
+            xEntrys.add(xData[i]);
+        }
+
+        //create the data set
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "STATISTICS");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        //add colors to dataset
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.BLUE);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.CYAN);
+        pieDataSet.setColors(colors);
+
+        //add legend to chart
+        Legend legend = pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+
+        //create pie data object
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+
+
+
+
     }
 
 }
